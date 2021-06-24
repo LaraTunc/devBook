2bffa16c725d78553ea2770ed368dda12a500a89
Lara Tunc <laratunc@gmail.com>
2021-06-24 16:25:13 -0400

    redux & auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
-import './App.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
+// css
+import './App.css';
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
 
 const App = () => {
+	// this will run only when mounted
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<BrowserRouter>
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,12 @@
-import { REGISTER_FAIL, REGISTER_SUCCESS } from '../actions/types';
+import {
+	AUTH_ERROR,
+	LOGIN_FAIL,
+	LOGIN_SUCCESS,
+	LOGOUT,
+	REGISTER_FAIL,
+	REGISTER_SUCCESS,
+	USER_LOADED,
+} from '../actions/types';
 
 const initialState = {
 	token: localStorage.getItem('token'),
@@ -11,7 +19,15 @@ export default function auth(state = initialState, action) {
 	const { type, payload } = action;
 
 	switch (type) {
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				user: payload,
+			};
 		case REGISTER_SUCCESS:
+		case LOGIN_SUCCESS:
 			localStorage.setItem('token', payload.token);
 			return {
 				...state,
@@ -20,6 +36,9 @@ export default function auth(state = initialState, action) {
 				loading: false,
 			};
 		case REGISTER_FAIL:
+		case AUTH_ERROR:
+		case LOGIN_FAIL:
+		case LOGOUT:
 			localStorage.removeItem('token');
 			return {
 				...state,
@@ -27,6 +46,7 @@ export default function auth(state = initialState, action) {
 				isAuthenticated: false,
 				loading: false,
 			};
+
 		default:
 			return state;
 	}
